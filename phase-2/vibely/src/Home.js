3f1a6a1d0f8c1d3a4abb1beac731fb5e0342637b
Bruce <bruce.hillsworth@gmail.com>
2023-04-24 16:45:46 -0600

    Fixed fetch requests

--- phase-2/vibely/src/Home.js
+++ phase-2/vibely/src/Home.js
@@ -2,19 +2,31 @@ import React, { useState, useEffect } from "react";
 
 function Home() {
   const [img, setImg] = useState("");
-  const [quote, setQuote] = useState([]);
+  const [quotes, setQuotes] = useState([]);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    fetch("https://picsum.photos/500").then((r) => setImg(r.url));
-    fetch("https://api.goprogram.ai/inspiration")
-      .then((r) => r.json())
-      .then((data) => setQuote(data.quote));
+    setIsMounted(true);
+    return () => setIsMounted(false);
   }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      fetch("https://picsum.photos/500").then((r) => setImg(r.url));
+      fetch("https://quote-garden.onrender.com/api/v3/quotes/random?count=3")
+        .then((r) => r.json())
+        .then(({ data }) => setQuotes(data));
+    }
+  }, [isMounted]);
+
+  const quotesToDisplay = quotes.map(({ id, quoteText }) => {
+    return <p key={id}>{quoteText}</p>;
+  });
 
   return (
     <div>
       <img src={img}></img>
-      <p>{quote}</p>
+      <div class="quotes">{quotesToDisplay}</div>
     </div>
   );
 }
