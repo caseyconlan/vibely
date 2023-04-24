dca0bed2a1033d35b8f19edbab995b293b7b1083
Bruce <bruce.hillsworth@gmail.com>
2023-04-24 11:16:27 -0600

    Added fetch request

--- phase-2/vibely/src/App.js
+++ phase-2/vibely/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 
 function App() {
+  console.log(process.env.REACT_APP_API_KEY);
+
+  useEffect(() => {
+    fetch(
+      `https://theaudiodb.com/api/v1/json/${process.env.REACT_APP_API_KEY}/searchalbum.php?s=daft_punk`
+    )
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
