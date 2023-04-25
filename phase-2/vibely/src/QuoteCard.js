885b0e2efad77b467d0baca3fa7714ecb0214e58
Bruce <bruce.hillsworth@gmail.com>
2023-04-25 16:04:12 -0600

    Added Keep and Change button

--- phase-2/vibely/src/QuoteCard.js
+++ phase-2/vibely/src/QuoteCard.js
@@ -1,8 +1,12 @@
 function QuoteCard({ id, quoteText, quoteAuthor }) {
   return (
     <div className="quote-container">
-      <p className="quote-text">{quoteText}</p>
-      <p className="quote-author">- {quoteAuthor}</p>
+      <button className="keepBtn">Keep</button>
+      <div className="quote">
+        <p className="quote-text">{quoteText}</p>
+        <p className="quote-author">- {quoteAuthor}</p>
+      </div>
+      <button className="changeBtn">Change</button>
     </div>
   );
 }
