e6927f8375d02176b8174475cda16e30c5179699
Casey <caseyconlanshopping@gmail.com>
2023-04-26 16:51:54 -0700

    Home button and colors

--- phase-2/vibely/src/NavBar.js
+++ phase-2/vibely/src/NavBar.js
@@ -2,16 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const activeStyle = { backgroundColor: "yellowgreen"};
+
   return (
     <div className="navbar">
-      <div id="vibely">Quotely</div>
+        <Link to="/"><div id="vibely">Quotely</div></Link>
       <div className="navbar-flex-container">
-        <div className="flex-item">Home</div>
         <div className="flex-item">
-          <Link to="/about">About</Link>
-          </div>
+          <Link to="/" activeStyle={activeStyle}>Home</Link>
+        </div>
         <div className="flex-item">
-          <Link to="/favorites">Favorites</Link>
+          <Link to="/about" activeStyle={activeStyle}>About</Link>
+        </div>
+        <div className="flex-item">
+          <Link to="/favorites" activeStyle={activeStyle}>Favorites</Link>
         </div>
       </div>
     </div>
