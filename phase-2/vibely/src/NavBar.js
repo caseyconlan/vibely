0d027fa2d8f39bcc244dae008768daf0fb1db8a2
Casey <caseyconlanshopping@gmail.com>
2023-04-26 14:44:15 -0700

    Home and Quotely Buttons

--- phase-2/vibely/src/NavBar.js
+++ phase-2/vibely/src/NavBar.js
@@ -4,12 +4,16 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className="navbar">
-      <div id="vibely">Quotely</div>
+      <Link to="/" className="navbar-brand">
+        Quotely
+      </Link>
       <div className="navbar-flex-container">
-        <div className="flex-item">Home</div>
+        <div className="flex-item">
+          <Link to="/">Home</Link>
+        </div>
         <div className="flex-item">
           <Link to="/about">About</Link>
-          </div>
+        </div>
         <div className="flex-item">
           <Link to="/favorites">Favorites</Link>
         </div>
